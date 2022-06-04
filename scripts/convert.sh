d8defa7bc817f4dc79f053802aa04045d2d6b556
ZestOfLife <jamescook371@gmail.com>
2022-06-05 03:52:56 +0700

    Heavily decouple components

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Set frames to 4:3 aspect ratio
-
-eval "mogrify frames-$2/*.jpg -gravity center -crop 4:3 frames-$2/*.jpg"
+# Get frame folder
+frame_name=$(basename -- "$1")
+frame_name="${frame_name%.*}"
 
 # Convert into gif and move to host path
 
-eval "convert frames-$2/*.jpg videos/$1"
+eval "convert videos/$2 videos/frames-$frame_name/$3 videos/$2"
--- scripts/make_thumbnail.sh
+++ scripts/make_thumbnail.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Make dir to store frames
+# Get frame folder
 frame_name=$(basename -- "$1")
 frame_name="${frame_name%.*}"
-eval "mkdir frames-${frame_name}"
 
 # Get frames
-eval "scripts/frames.sh $1 $frame_name"
+eval "scripts/frames.sh $1 $2"
+
+# Resize frames
+eval "scripts/resize.sh $1 *.jpg"
 
 # Convert frames
-eval "scripts/convert.sh $2 $frame_name"
+eval "scripts/convert.sh $1 $2 *.jpg"
+
+# Cleanup
+eval "rm -r videos/frames-$frame_name"
