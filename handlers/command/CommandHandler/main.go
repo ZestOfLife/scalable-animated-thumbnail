bdf95ac4d6b5ac66d340e4b6fca9335a05e82a29
ZestOfLife <jamescook371@gmail.com>
2022-06-14 01:25:35 +0700

    Add http server to get requests from

---
+++ handlers/command/CommandHandler/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/reportnewjob", JobHandler)
+	http.HandleFunc("/reportextract", ExtractSuccessHandler)
+	http.HandleFunc("/reportresize", ResizeSuccessHandler)
+	http.HandleFunc("/reportcompile", CompileSuccessHandler)
+	http.HandleFunc("/reportextractfailure", ExtractFailureHandler)
+	http.HandleFunc("/reportresizefailure", ResizeFailureHandler)
+	http.HandleFunc("/reportcompilefailure", CompileFailureHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
